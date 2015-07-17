aaa7cecfcd7086cecb76ad2706d1cc10f6ec85c4
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-07-16 22:06:29 -0400

    Saving article parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all    
@@ -10,5 +11,11 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 end
